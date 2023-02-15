1992a48f85feeabdf1f2a18a3b03fbd51f81001e
Nahian Rifaat <nrifaat26@gmail.com>
2023-02-15 11:11:06 +0600

    New commit after cap deployment config 6 added factory bot in development and test environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.factory_bot.defintion_file_paths = ['l']
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
