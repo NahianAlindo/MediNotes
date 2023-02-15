21cf581519941e52acf94ead5759e7e08d7f68ab
Nahian Rifaat <nrifaat26@gmail.com>
2023-02-15 12:54:24 +0600

    New commit to reset migrations for deploy

--- db/migrate/20230115122804_create_dosages.rb
+++ db/migrate/20230115122804_create_dosages.rb
@@ -1,5 +1,11 @@
 class CreateDosages < ActiveRecord::Migration[6.0]
   def change
+    create_table :patients do |t|
+      t.string :name
+      t.string :contact_number
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
     create_table :dosages do |t|
       t.datetime :timestamp
       t.integer :blood_sugar_level
